563b8771d9305fce3cc9a52d1a4c42b748cad4f6
Fernando <ferobregon03@gmail.com>
2022-09-29 23:24:02 +0100

    graph created

---
+++ src/components/Graph.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let width = 500;
+  export let height = 500;
+
+  import { ForceGraph } from '../utils/forceGraph';
+  import * as miserables from '../miserables.json';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    const chart = ForceGraph(miserables.default, {
+      nodeId: (d) => d.id,
+      nodeGroup: (d) => d.group,
+      nodeTitle: (d) => `${d.id}\n${d.group}`,
+      linkStrokeWidth: (l) => Math.sqrt(l.value),
+      width,
+      height,
+    });
+    const div = document.querySelector('#network');
+    div.appendChild(chart);
+  });
+</script>
+
+<section>
+  <div id="network" class="" />
+</section>
